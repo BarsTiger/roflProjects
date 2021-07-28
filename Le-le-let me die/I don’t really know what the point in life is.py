bc34b96aa2bba2488730458f232b247e0fb27593
BarsTiger <kit.barsic@gmail.com>
2021-07-28 22:02:23 +0300

    1

--- Le-le-let me die/I don’t really know what the point in life is.py
+++ Le-le-let me die/I don’t really know what the point in life is.py
@@ -1,4 +1,4 @@
-import time, subprocess, sys
+import time, subprocess, sys, os
 
 try:
     import vlc
@@ -9,7 +9,10 @@ except:
 toxin = vlc.MediaPlayer("https://github.com/BarsTiger/roflProjects/raw/master/files/toxin.mp3")
 toxin.play()
 
-time.sleep(16)
+time.sleep(1.5)
+os.system('cls' if os.name == 'nt' else 'clear')
+
+time.sleep(17)
 print("Я...")
 time.sleep(2)
 print("Гуль")
